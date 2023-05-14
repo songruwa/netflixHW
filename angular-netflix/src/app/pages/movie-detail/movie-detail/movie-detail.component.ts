4de96f634b02cdf83aab39baf207322049b92821
Songru Wang <michaelsong6918@gmail.com>
2023-05-14 14:52:01 -0700

    youtube-player achieved

--- angular-netflix/src/app/pages/movie-detail/movie-detail/movie-detail.component.ts
+++ angular-netflix/src/app/pages/movie-detail/movie-detail/movie-detail.component.ts
@@ -5,8 +5,7 @@ import { MovieDetail } from '../../../movie-detail.interface';
 import { MatDialog } from '@angular/material/dialog'; 
 import { Video } from '../video.interface';
 import { Movie } from 'src/app/movie.interface';
-
-
+import { YoutubeComponent } from '../youtube/youtube.component';
 
 
 @Component({
@@ -30,28 +29,29 @@ export class MovieDetailComponent implements OnInit{
     this.route.params.subscribe(params => {
       this.movieId = +params['id'];
       this.movieService.getMovieDetail(this.movieId).subscribe(movie => {
-        console.log(this.movieId);
         this.moviedetail = movie;
         this.movie = movie;
       });
     });
   }
 
-  // openDialog(): void { // Add openDialog method
-  //   const dialogRef = this.dialog.open(YoutubeComponent, {
-  //     data: {
-  //       movieVideos: this.movieVideos, 
-  //       hasposter_img: this.hasPoster_img, 
-  //       hasbackdrop_img: this.hasBackdrop_img,
-  //       poster_img_high: this.poster_img_high, 
-  //       backdrop_img_high: this.backdrop_img_high,
-  //     },
-  //     backdropClass: 'backdropBackground',
-  //     panelClass: 'my-panel',
-  //   });
+  // https://www.nerd.vision/post/how-to-pass-data-to-a-matdialog
+  openDialog(): void { // Add openDialog method
+    const dialogRef = this.dialog.open(YoutubeComponent, {
+      data: {
+        movieId: this.movieId,
+        movieVideos: this.movieVideos, 
+        hasposter_img: this.hasPoster_img, 
+        hasbackdrop_img: this.hasBackdrop_img,
+        poster_img_high: this.poster_img_high, 
+        backdrop_img_high: this.backdrop_img_high,
+      },
+      backdropClass: 'backdropBackground',
+      panelClass: 'my-panel',
+    });
 
-  //   dialogRef.afterClosed().subscribe((result) => {
-  //     console.log('The dialog was closed', result);
-  //   });
-  // }
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed', result);
+    });
+  }
 }
--- angular-netflix/src/app/pages/movie-detail/youtube/youtube.component.ts
+++ angular-netflix/src/app/pages/movie-detail/youtube/youtube.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MovieService } from '../../../service/movie.service';
 
 @Component({
   selector: 'app-youtube',
   templateUrl: './youtube.component.html',
   styleUrls: ['./youtube.component.css']
 })
-export class YoutubeComponent {
+export class YoutubeComponent implements OnInit {
 
+  keys: string[] = [];
+  hasPoster_img = true;
+  hasBackdrop_img = true;
+  poster_img_high = '';
+  backdrop_img_high = '';
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private movieService: MovieService) { }
+
+  ngOnInit(): void {
+    this.hasPoster_img = this.data.hasPoster_img;
+    this.hasBackdrop_img = this.data.hasBackdrop_img;
+    this.poster_img_high = this.data.poster_img_high;
+    this.backdrop_img_high = this.data.backdrop_img_high;
+
+    if (this.data.movieId) {
+      this.movieService.getMovieVideo(this.data.movieId).subscribe((keys) => {
+        this.keys = keys;
+        console.log(this.keys);
+      });
+    }
+
+    const tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    document.body.appendChild(tag);
+  }
+
+  switchVideo(direction: string) {
+    if (direction === 'left' && this.keys.length) {
+      const removedKey: string | undefined = this.keys.shift();
+      if (removedKey) {
+        this.keys.push(removedKey);
+      }
+    } else if (direction === 'right' && this.keys.length) {
+      const removedKey: string | undefined = this.keys.pop();
+      if (removedKey) {
+        this.keys.unshift(removedKey);
+      }
+    }
+  }
 }
